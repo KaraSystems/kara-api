072141b5c0456d1aaaffb5c174018ce0de9f3d16
Jerferson <jerfersongriza@gmail.com>
2021-08-04 22:58:26 -0300

    chore: add config sonar

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
   ],
+  reporters: ['default',  'jest-sonar'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   roots: ['<rootDir>/src'],
