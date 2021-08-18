1d46a97c17094b2d9990467b42540689aa865c00
Jerferson <jerfersongriza@gmail.com>
2021-08-18 08:21:31 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,5 +49,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('should return 401 on login ', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'gaara@areia.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
