207f847bcdd81c4d05b3a39817b29d510cea9243
Jerferson <jerfersongriza@gmail.com>
2021-08-22 13:50:22 -0300

    Revert "test: ensure DbAddSurvey throws if AddSurveyRepository throws"
    This reverts commit ad90cf104c298cc0c661b48b178a846a5bac43ce.

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -40,12 +40,4 @@ describe('DbAddSurvey Usecase', () => {
     await sut.add(surveyData)
     expect(addSpy).toHaveBeenCalledWith(surveyData)
   })
-
-  it('should throw if AddSurveyRepository throw', async () => {
-    const { sut, addSurveyRepositoryStub } = makeSut()
-    jest.spyOn(addSurveyRepositoryStub, 'add').mockReturnValueOnce(Promise.reject(new Error()))
-
-    const promise = sut.add(makeFakeSurveyData())
-    await expect(promise).rejects.toThrow()
-  })
 })
