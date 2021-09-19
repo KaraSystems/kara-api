680e6b9a4a11eded851320bc9c17864a0fcdc84d
Jerferson <jerfersongriza@gmail.com>
2021-09-19 10:26:47 -0300

    feat: ensure AccountMongoRepository find an account by token without role

--- src/main/factories/controllers/login/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login/login-controller-factory.ts
@@ -1,8 +1,8 @@
-import { LoginController } from '../../../../../presentation/controllers/access/login/login-controller'
 import { makeLoginValidation } from './login-validation-factory'
 import { Controller } from '../../../../../presentation/protocols'
 import { makeDbAuthentication } from '../../../usecases/account/authentication/db-authentication-factory'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { LoginController } from '../../../../../presentation/controllers/access/login/login-controller'
 
 export const makeLoginController = (): Controller => {
   const controller = new LoginController(makeDbAuthentication(), makeLoginValidation())
