802dfca3df77196ba133e129c5864a3cd09eefdd
Jerferson <jerfersongriza@gmail.com>
2021-10-16 19:03:28 -0300

    test(survey): ensure DbLoadSurveyById returns a Survey if LoadSurveyByIdRepository success

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -55,4 +55,12 @@ describe('DbLoadSurveyById', () => {
 
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
   })
+
+  test('Should return Survey when calls with correct id', async () => {
+    const { sut } = makeSut()
+
+    const survey = await sut.loadById('any_id')
+
+    expect(survey).toEqual(makeFakeSurvey())
+  })
 })
