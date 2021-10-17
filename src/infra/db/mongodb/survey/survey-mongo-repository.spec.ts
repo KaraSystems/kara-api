fe6dae62af9b8896866f4fa5673a1890f807f6b4
Jerferson <jerfersongriza@gmail.com>
2021-10-17 07:42:30 -0300

    test(survey): ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -79,4 +79,24 @@ describe('Survey Mongo Repository', () => {
       expect(surveys.length).toBe(0)
     })
   })
+
+  describe('loadById()', () => {
+    it('Should load survey by id on success', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'any_questions',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      })
+      const sut = makeSut()
+
+      const surveys = await sut.loadById(res.ops[0]._id)
+
+      expect(surveys.question).toBe('any_questions')
+    })
+  })
 })
